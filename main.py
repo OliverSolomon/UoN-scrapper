1a5fffc6699434f412ea03bdf55d7f3cbbb75dcb
OliverSolomon <oliversolomonwainaina@gmail.com>
2021-02-17 17:09:28 +0300

    basic config

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
+"""This script populates fields with user data generated """
+
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support.select import Select
+from time import sleep
+from requests import get
+
+from data import uname, passwd
+
+# def getText():
+
+url = r"https://multimedia.uonbi.ac.ke/"
+
+#initiated driver
+driver = webdriver.Chrome(executable_path="/home/gamin3/Desktop/Ghosts-main/Ghosts-beta/chromedriver/chromedriverV88.0.4324.150")
+
+
+# +++++++++++++++++++++    XPATHS    +++++++++++++++++++++++++
+unameXpath = ''
+passwdXpath = ''
+formSubmitXpath = ''
+
+
+# +++++++++++++++++++++    FUNCTIONALITY    ++++++++++++++++++
+
+#initialising
+driver.get(url)
+
+#log in page
+driver.find_element_by_xpath(unameXpath).send_keys()
+driver.find_element_by_xpath(passwdXpath).send_keys()
+driver.find_element_by_xpath(formSubmitXpath).click()
